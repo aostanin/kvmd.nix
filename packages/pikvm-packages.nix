94acaac7effea85a159e5fbee5223184fb0ce457
aostanin <1375097+aostanin@users.noreply.github.com>
2026-08-30 03:18:22 +0000

    pikvm-packages: 0-unstable-2026-08-06 -> 0-unstable-2026-08-29

--- packages/pikvm-packages.nix
+++ packages/pikvm-packages.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "pikvm-packages";
-  version = "0-unstable-2026-08-06";
+  version = "0-unstable-2026-08-29";
 
   src = fetchFromGitHub {
     owner = "pikvm";
     repo = "packages";
-    rev = "e42612d7cbc67d77558893b67b5c76d271993d2e";
-    hash = "sha256-UnoUu5KU7dbjl7i750y43oO1JwKKIYc6RThTG1ha+wo=";
+    rev = "314cc6c36d722ffbd95aab0dd1af3cf934614ccd";
+    hash = "sha256-xeEn6yBPdpaHoCmCt74gXOfBcn9rmvf+mdJ3nwfL6Qk=";
   };
 
   dontConfigure = true;

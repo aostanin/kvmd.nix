abb2b83e9a8ab0f80b64f1e61de68f805d71e060
Philippe Laflamme <philippe.laflamme@gmail.com>
2026-09-08 18:00:24 -0400

    feat: replace nixos-hardware with nixos-raspberrypi
    This replaces `nixos-hardware` with `nixos-raspberrypi` which is
    specialized for deploying nixos configurations to that hardware.

--- modules/variants/v2-hdmi-rpi4.nix
+++ modules/variants/v2-hdmi-rpi4.nix
@@ -3,7 +3,11 @@
 
   services.kvmd.variant = "v2-hdmi-rpi4";
 
-  hardware.raspberry-pi."4".tc358743.enable = true;
+  hardware = {
+    raspberry-pi.config.all = {
+      dt-overlays."tc358743".enable = true;
+    };
+  };
 
   boot.kernelModules = ["tc358743"];
   boot.kernelParams = ["cma=192M"];
